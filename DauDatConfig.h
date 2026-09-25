272ba8c5a4741a531ce4e88285028833c7c1eac6
chuongnguyen89dn-ui <chuongnguyen89dn@gmail.com>
2026-09-25 21:36:21 +0700

    Preserve full DuoDash bridge and layout preference semantics

--- DauDatConfig.h
+++ DauDatConfig.h
@@ -1,17 +1,43 @@
 #import <Foundation/Foundation.h>
 
 static NSString * const DDPreferencesDomain = @"com.chuong.daudat.settings";
-static NSString * const DDLeftAppKey = @"daudat_split_left";
-static NSString * const DDRightAppKey = @"daudat_split_right";
-static NSString * const DDRatioKey = @"daudat_split_ratio";
-static NSString * const DDSplitEnabledKey = @"daudat_split_enabled";
+
+// Preserve the original DuoDash preference semantics under the new Đầu Đất namespace.
+// This lets the compatibility layer keep every layout/state dimension instead of collapsing
+// the project to a simple two-pane prototype.
+static NSString * const DDAppBridgeAutostartKey = @"appbridge_autostart";
+static NSString * const DDAppBridgeEnabledKey = @"appbridge_enabled";
+static NSString * const DDAppBridgeLayoutKey = @"appbridge_layout";
+static NSString * const DDSplitEnabledKey = @"appbridge_split_enabled";
+static NSString * const DDSplitFracAKey = @"appbridge_split_frac_a";
+static NSString * const DDSplitFracBKey = @"appbridge_split_frac_b";
+static NSString * const DDSplitFracLayoutKey = @"appbridge_split_frac_layout";
+static NSString * const DDLeftAppKey = @"appbridge_split_left";
+static NSString * const DDRatioKey = @"appbridge_split_ratio";
+static NSString * const DDRightAppKey = @"appbridge_split_right";
+static NSString * const DDThirdAppKey = @"appbridge_split_third";
+static NSString * const DDBridgedAppsKey = @"bridgedApps";
+static NSString * const DDNavBubbleDockModeKey = @"navbubble_dock_mode";
+static NSString * const DDNavProviderAutostartKey = @"navprovider_autostart";
+static NSString * const DDNavProviderSelectedKey = @"navprovider_selected";
 
 static NSDictionary *DDDefaultConfiguration(void) {
     return @{
+        DDAppBridgeAutostartKey:@YES,
+        DDAppBridgeEnabledKey:@YES,
+        DDAppBridgeLayoutKey:@2,
         DDSplitEnabledKey:@YES,
+        DDSplitFracAKey:@0,
+        DDSplitFracBKey:@0,
+        DDSplitFracLayoutKey:@0,
         DDLeftAppKey:@"com.google.ios.youtube",
+        DDRatioKey:@50,
         DDRightAppKey:@"com.waze.iphone",
-        DDRatioKey:@50
+        DDThirdAppKey:@"",
+        DDBridgedAppsKey:@[@"com.zhiliaoapp.musically", @"com.facebook.Facebook", @"com.waze.iphone", @"com.google.ios.youtube"],
+        DDNavBubbleDockModeKey:@0,
+        DDNavProviderAutostartKey:@NO,
+        DDNavProviderSelectedKey:@""
     };
 }
 
