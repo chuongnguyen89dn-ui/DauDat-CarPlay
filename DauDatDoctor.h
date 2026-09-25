0cc37684ff8a04a6e83f319d1a8dfada6e9b6ab3
chuongnguyen89dn-ui <chuongnguyen89dn@gmail.com>
2026-09-25 22:45:17 +0700

    Remove unused Doctor preference variable

--- DauDatDoctor.h
+++ DauDatDoctor.h
@@ -39,7 +39,6 @@ static NSString *DDDoctorSnapshot(BOOL fullscreen, UIWindow *statusBar) {
     NSDictionary *cfg=DDLoadConfiguration();
     [o appendFormat:@"Config: %@\nFullscreen: %d\n",cfg,fullscreen ? 1 : 0];
     NSUserDefaults *defaults=[NSUserDefaults standardUserDefaults];
-    NSDictionary *newDomain=[defaults persistentDomainForName:DDPreferencesDomain];
     [o appendFormat:@"Bridge: enabled=%@ autostart=%@ layout=%@ split=%@ ratio=%@ left=%@ right=%@ third=%@\n",
       cfg[DDAppBridgeEnabledKey],cfg[DDAppBridgeAutostartKey],cfg[DDAppBridgeLayoutKey],cfg[DDSplitEnabledKey],
       cfg[DDRatioKey],cfg[DDLeftAppKey],cfg[DDRightAppKey],cfg[DDThirdAppKey]];
